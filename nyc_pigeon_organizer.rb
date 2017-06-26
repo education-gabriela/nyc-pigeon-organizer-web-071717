1ad68eef82592287f47aad7c4de0b668c7e786f5
gabidavila <gabidavila@gmail.com>
2017-06-26 18:38:31 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # Gets all names
+  values = pigeons.values_at(*pigeons.keys).inject(:merge).values.flatten.uniq
+  # Also works with
+  # values = data.first.last.values.flatten.uniq
+
+  pigeons = {}
+
+  # Transform names to keys and creates pigeons hash
+  values.each do |name|
+    pigeons.store(name, {})
+
+    # Loop through all the data
+    data.each do |main_attr, information|
+      info = []
+      # Names inside each value
+      information.each do |attr, items|
+        if items.include?(name)
+          # Add it to the named pigeon
+          pigeons[name][main_attr] = info << attr.to_s
+        end
+      end
+    end
+  end
+  pigeons
 end
