7e7edad1a07a851163472de57cda351385124be7
gabidavila <gabidavila@gmail.com>
2017-06-27 18:07:51 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,30 @@
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
   # Gets all names
-  values = pigeons.values_at(*pigeons.keys).inject(:merge).values.flatten.uniq
-  # Also works with
-  # values = data.first.last.values.flatten.uniq
-
+  values = data.values.collect(&:values).flatten.uniq
   pigeons = {}
 
   # Transform names to keys and creates pigeons hash
   values.each do |name|
-    pigeons.store(name, {})
+    pigeons[name] = {}
 
     # Loop through all the data
     data.each do |main_attr, information|
@@ -17,9 +33,10 @@ def nyc_pigeon_organizer(data)
       information.each do |attr, items|
         if items.include?(name)
           # Add it to the named pigeon
-          pigeons[name][main_attr] = info << attr.to_s
+          info = info << attr.to_s
         end
       end
+      pigeons[name][main_attr] = info
     end
   end
   pigeons
